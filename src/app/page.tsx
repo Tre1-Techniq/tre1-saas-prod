0807476310c768e73ebf021cf08fde31a3f23da5
Carlos Anderson <carlos@carlos-anderson.com>
2024-05-21 13:59:57 -0700

    COnnected to Drizzle Studio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/015f052d-4718-4242-afc1-0f815de1edf6-87uj8k.jpg",
@@ -15,10 +16,16 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log("DB POSTS: ", posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
