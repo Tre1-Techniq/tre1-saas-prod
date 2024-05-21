6427850fb83e5754ac2e32e9ab7ee6784ab63644
Carlos Anderson <carlos@carlos-anderson.com>
2024-05-21 15:30:51 -0700

    Added Clerk Auth to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     // where: (model, { eq }) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.id),
   });
 
-  console.log("DB POSTS: ", images);
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex flex-col w-48">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex flex-col w-48">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
