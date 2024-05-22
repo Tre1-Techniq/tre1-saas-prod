8f0fccd90ea8ee2a64235a260b0b346f2636b877
Carlos Anderson <carlos@carlos-anderson.com>
2024-05-21 17:48:41 -0700

    Separated Queries to Server-Only

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../../utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 export function NavBar () {
+    const router = useRouter();
     return (
         <nav className="flex items-center justify-between w-full border-b p-4 text-xl font-semibold">
             <div>TRE1 LOGO</div>
-            <div>
+            <div className="flex flex-row">
                 <SignedOut>
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
+                    <UploadButton 
+                        endpoint="imageUploader"
+                        onClientUploadComplete={() => {
+                            router.refresh();
+                        }}
+                    />
                     <UserButton />
                 </SignedIn>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/db/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    // where: (model, { eq }) => eq(model.userId, user.userId),
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
